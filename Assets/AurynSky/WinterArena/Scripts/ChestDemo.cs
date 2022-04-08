5c1d7ec340d17c8320adb32296028b72a3a7ae87
GoktugEray <eray.yilmaz03@bilgiedu.net>
2022-04-09 02:21:57 +0300

    first commit

---
+++ Assets/AurynSky/WinterArena/Scripts/ChestDemo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestDemos : MonoBehaviour
+{
+
+    //This script goes on the ChestComplete prefab;
+
+    public Animator chestAnims; //Animator for the chest;
+
+    // Use this for initialization
+    void Awake()
+    {
+        //get the Animator component from the chest;
+        chestAnims = GetComponent<Animator>();
+        //start opening and closing the chest for demo purposes;
+        StartCoroutine(OpenCloseChests());
+    }
+
+
+    IEnumerator OpenCloseChests()
+    {
+        //play open animation;
+        chestAnims.SetTrigger("open");
+        //wait 2 seconds;
+        yield return new WaitForSeconds(2);
+        //play close animation;
+        chestAnims.SetTrigger("close");
+        //wait 2 seconds;
+        yield return new WaitForSeconds(2);
+        //Do it again;
+        StartCoroutine(OpenCloseChests());
+
+    }
+}
